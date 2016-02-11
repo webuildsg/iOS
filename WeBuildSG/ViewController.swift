1f74e0abdd849d9a445d4dbd52b0f7f9d7dede66
Sayanee <sayanee@gmail.com>
2016-02-11 13:46:19 +0800

    feat: testing out dynamic row height

--- WeBuildSG/ViewController.swift
+++ WeBuildSG/ViewController.swift
@@ -12,8 +12,9 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         SectionsData().getSectionsFromData({
             sections in
             
+            self.tableView.estimatedRowHeight = self.tableView.rowHeight
+            self.tableView.rowHeight = UITableViewAutomaticDimension
             self.sections = sections
-            
             self.tableView.reloadData()
         })
     }
