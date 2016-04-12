1243bba130eca0cf960f5f230a1c352ac2de29fd
Sayanee <sayanee@gmail.com>
2016-04-12 17:08:14 +0800

    feat: add 3 static cells. #1

--- WeBuildSG/DetailTableViewController.swift
+++ WeBuildSG/DetailTableViewController.swift
@@ -57,12 +57,12 @@ class DetailTableViewController: UITableViewController {
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 3
     }
 
     /*
